f5fc0f2a87cec85e19928972281d57f84756d56e
Omar Mohamed Elsayed <elsharkawy159.om@gmail.com>
2023-11-18 21:07:11 +0200

    Update PageHeader.jsx

--- Components/partials/PageHeader.jsx
+++ Components/partials/PageHeader.jsx
@@ -1,15 +1,24 @@
-
-const PageHeader = ({title, subTitle, bg}) => {
+const PageHeader = ({ title, subTitle, bg }) => {
   return (
     <div
       className="page-header text-center py-5"
-      style={{ backgroundImage: `url(${bg || "/img/WhiteBG.jpg" || "../../public/img/whiteBG.jpg" || "img/WhiteBG.jpg"})`, backgroundPosition: "center", backgroundRepeat: "no-repeat", backgroundSize: "cover"}}
+      style={{
+        backgroundImage: `url(${
+          bg ||
+          "/img/WhiteBG.jpg" ||
+          "../../public/img/whiteBG.jpg" ||
+          "img/WhiteBG.jpg"
+        })`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
     >
       <div className="container">
         <h2 className="text-center d-flex flex-column text-uppercase m-0 text-main fw-semibold">
           {title}
         </h2>
-          <span>{subTitle}</span>
+        <span>{subTitle}</span>
       </div>
     </div>
   );
