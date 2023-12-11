f7ba741099cc3fe3e5eca527515bb3f158cbffab
Omar Mohamed Elsayed <111247064+elsharkawy159@users.noreply.github.com>
2023-12-11 23:06:27 +0200

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import PageHeader from '../Components/partials/PageHeader.jsx'
-import BannerAd from '../Components/partials/BannerAd.jsx';
+import React from "react";
+import PageHeader from "../Components/partials/PageHeader.jsx";
+import BannerAd from "../Components/partials/BannerAd.jsx";
 
 const about = () => {
   return (
@@ -9,7 +9,7 @@ const about = () => {
       <div className="container py-5">
         <div className="row">
           <div className="col-md-6">
-            <h2 className='text-main'>Who We Are?</h2>
+            <h2 className="text-main">Who We Are?</h2>
             <p>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet
               voluptatem fuga necessitatibus asperiores ad molestiae quasi, sunt
@@ -20,12 +20,12 @@ const about = () => {
             </p>
           </div>
           <div className="col-md-6">
-            <img src="/img/mainSlider/slide-12.jpg" className='w-100' alt="" />
+            <img src="/img/mainSlider/slide-12.jpg" className="w-100" alt="" />
           </div>
         </div>
       </div>
     </>
   );
-}
+};
 
-export default about
+export default about;
