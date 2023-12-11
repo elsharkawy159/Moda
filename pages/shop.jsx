2148c9bea94b118f414704d6597aa7def88ff771
Omar Mohamed Elsayed <111247064+elsharkawy159@users.noreply.github.com>
2023-12-12 00:34:29 +0200

    Update shop.jsx

--- pages/shop.jsx
+++ pages/shop.jsx
@@ -139,14 +139,14 @@ const Shop = () => {
 
           {/* Product display */}
           <div className="col-md-9 shadow-4 border border-top-0 border-bottom-0">
-            <div className="row justify-content-center align-items-center h-100">
-              <ul>
-                {Object.entries(filters).map(([key, value]) => (
-                  <li key={key}>
-                    {key}: {JSON.stringify(value)}
-                  </li>
-                ))}
-              </ul>
+            <ul className="d-flex">
+              {Object.entries(filters).map(([key, value]) => (
+                <li className="mx-1" key={key}>
+                  {key}: {JSON.stringify(value)}
+                </li>
+              ))}
+            </ul>
+            <div className="row justify-content-center text-center h-100">
               {productData.productsCount === 0 ? (
                 <h3>No Products Found</h3>
               ) : (
